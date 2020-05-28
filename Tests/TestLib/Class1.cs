7cdb1a38ee2d09ccb1e590486016161f76d827b1
GeoCod <artem.g.vasilchenko@gmail.com>
2020-05-28 21:25:09 +0300

    Тестируем использование элементов WPF в библиотеках

---
+++ Tests/TestLib/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TestLib
+{
+    public abstract class Command : ICommand
+    {
+        /// <summary>Событие когда команда меняет состояние</summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        /// <summary>Проверяет можно или нельзя выполнять коммандну</summary>
+        /// <param name="parameter"></param>
+        public abstract bool CanExecute(object parameter);
+
+        /// <summary>То, что должно быть выоплнено коммандой</summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object parameter);
+    }
+}
