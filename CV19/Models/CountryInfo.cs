af3e0af08e235e09d06782bf02362195e70864ad
GeoCod <artem.g.vasilchenko@gmail.com>
2020-06-02 10:33:39 +0300

    Добавляем модели

---
+++ CV19/Models/CountryInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CV19.Models
+{
+    /// <summary>Информация о стране</summary>
+    internal class CountryInfo : PlaceInfo
+    {
+        /// <summary>Перечисление провинций входящих в страну</summary>
+        public IEnumerable<ProvinceInfo> ProvinceCounts { get; set; }
+    }
+}
---
+++ CV19/Models/PlaceInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace CV19.Models
+{
+    /// <summary>Базовый класс для стран и провинций</summary>
+    internal class PlaceInfo
+    {
+        /// <summary>Название страны</summary>
+        public string Name { get; set; }
+
+        /// <summary>Координаты страны</summary>
+        public Point Location { get; set; }
+
+        /// <summary>Количество подтвержденных заражений</summary>
+        public IEnumerable<ConfirmedCount> Counts { get; set; }
+    }
+}
